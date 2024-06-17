6dc39bc2c968785c2ef8c314512460fefb643868
Danis Cindrak <dcindrak@z3r3p1.42lyon.fr>
2024-06-17 15:53:45 +0200

    finish

--- command/User.cpp
+++ command/User.cpp
@@ -9,11 +9,23 @@ void user_execute(std::vector<std::string> &params, int client_fd, std::map<int,
         send(client_fd, response, strlen(response), 0);
         return;
     }
-    if (params.size() >= 4)
+    if (params.size() ==4)
     {
         std::string realname = params[3];
         if (realname[0] == ':') {
             realname.erase(0, 1);
+        }
+        if (params[0].empty() || params[1].empty() || params[2].empty() || realname.empty())
+        {
+            const char *response = ":monserver 461 * USER :Not enough parameters\r\n";
+            send(client_fd, response, strlen(response), 0);
+            return;
+        }
+        if (params[1].find_first_not_of("0123456789") != std::string::npos)
+        {
+            const char *response = ":monserver 461 * USER :Not enough parameters\r\n";
+            send(client_fd, response, strlen(response), 0);
+            return;
         }
         if (realname.empty())
         {
